c74bc2991e3597188390d84d12579069d9f9413f
Quan1u2u <165462713+Quan1u2u@users.noreply.github.com>
2025-12-25 16:26:16 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ VIP_FILE_PATH = "vip_users.json"      # File lưu danh sách VIP
 BACKGROUND_IMAGE_NAME = "background.jpg"
 
 # DANH SÁCH ADMIN (ID)
-ADMIN_IDS = ["250231", "250218", "admin"]
+ADMIN_IDS = ["250231", "250218", "000000"]
 
 # --- LUẬT CHƠI (STANDARD vs VIP) ---
 STD_MAX_QUESTIONS = 3   # Thường: 3 câu
@@ -637,5 +637,6 @@ if prompt := st.chat_input("Nhập câu hỏi gợi ý hoặc đoán tên..."):
                 st.rerun()
 
     except Exception as e: st.error(f"Lỗi: {e}")
+
 
 
